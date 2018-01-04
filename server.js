c0acd1ae56151b6859295f65b9fb1feb6addaac7
simonidnov <sdelamarre@idnovant.fr>
2018-01-04 14:20:19 +0100

    try add child process

--- server.js
+++ server.js
@@ -136,8 +136,8 @@ io.on('connection', function(socket){
                   console.log("omxplayer -o local http://10.3.141.1:3000/"+datas.file);
 
                   cp.exec("export DISPLAY=:0", function(error, stdout, stderr) {});
-                  
-                  cp.exec("omxplayer -o local http://10.3.141.1:3000/"+datas.file, function(error, stdout, stderr) {
+
+                  cp.exec("omxplayer -o local "+datas.file, function(error, stdout, stderr) {
                       if (stderr !== null) {
                           console.log("exec errror: " + error);
                       }
