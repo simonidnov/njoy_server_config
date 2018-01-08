fd1fa330045698fab9300998d56ebea6d0aa14bc
simonidnov <sdelamarre@idnovant.fr>
2018-01-08 10:59:14 +0100

    drawing test

--- src/pages/drawing/drawing.js
+++ src/pages/drawing/drawing.js
@@ -146,7 +146,7 @@ drawer.prototype.update = function() {
         this.stage.update();
         console.log(this.pencil);
         this._removed = [];
-        if(new Date().getTime() - this.lastSend > 20){
+        if(new Date().getTime() - this.lastSend > 50){
           app.socket.emit("njoy", {
             "status":"drawing",
             "color":createjs.Graphics.getRGB(0,0,0),
