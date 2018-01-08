446b830c209f4acc3f6022876b9a90de86216bf0
simonidnov <sdelamarre@idnovant.fr>
2018-01-08 12:28:09 +0100

    drawing test

--- src/pages/drawing/drawing.js
+++ src/pages/drawing/drawing.js
@@ -43,10 +43,14 @@ var drawer = function(canvas_id){
   */
   var self = this;
   $('[data-color]').off('click').on('click', function(){
+    console.log('color ::: ', $(this).attr('data-color'));
     self.color = $(this).attr('data-color');
+    self.pencil.color = $(this).attr('data-color');
   });
   $('[data-size]').off('click').on('click', function(){
+    console.log('size ::: ', $(this).attr('data-size'));
     self.size = $(this).attr('data-size');
+    self.pencil.size = $(this).attr('data-size');
   });
   this.canvas.width = 1920;
   this.canvas.height = 1080;
