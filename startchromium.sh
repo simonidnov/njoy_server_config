aa2ff097ad52393498e4eb576cde2a442ed6c2f1
simonidnov <sdelamarre@idnovant.fr>
2018-01-23 18:04:32 +0100

    startchromium

---
+++ startchromium.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#sleep 10
+killall chromium-browser
+export DISPLAY=:0
+chromium-browser --noerrdialogs --disable-infobars --disable-session-crashed-bu$
+
+#chromium-browser --start-maximized --noerrdialogs --disable-infobars --disable$
+
+#sudo amixer cset numid=1<<njoynjoy
+#sleep 15
+#xte "key F11" -x:0
+
+#chromium-browser --kiosk http://10.3.141.1:3000/receptor
+#sleep 10
+#epiphany http://10.3.141.1:3000/receptor --display=:0 &
+#sleep 15
+#xte "key F11" -x:0
