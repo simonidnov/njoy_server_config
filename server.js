226d42750f71562f6af4e368c1ffe6f6e718e870
simonidnov <sdelamarre@idnovant.fr>
2018-01-30 11:37:41 +0100

    server start chromium

--- server.js
+++ server.js
@@ -59,7 +59,7 @@ io.on('connection', function(socket){
             call = "njoy";
         switch(datas.status){
             case 'reboot':
-              cp.exec("home/pi/njoy/startchromium.sh", function(error, stdout, stderr) {
+              cp.exec("/home/pi/njoy/startchromium.sh", function(error, stdout, stderr) {
                 console.log("stdout: " + stdout);
                 console.log("stderr: " + stderr);
                 if (error !== null) {
@@ -270,7 +270,7 @@ io.on('connection', function(socket){
 
 http.listen(port, function(){
   console.log('listening on *:' + port);
-    cp.exec("/njoy/startchromium.sh", function(error, stdout, stderr) {
+    cp.exec("/home/pi/njoy/startchromium.sh", function(error, stdout, stderr) {
         console.log("stdout: " + stdout);
         console.log("stderr: " + stderr);
         if (error !== null) {
