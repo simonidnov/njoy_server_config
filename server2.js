32bed9784cd196500a2562b56d1264bfebb62eb6
simonidnov <sdelamarre@idnovant.fr>
2018-04-20 10:18:59 +0200

    video is playing

--- server2.js
+++ server2.js
@@ -127,12 +127,14 @@ io.on('connection', function(socket){
                   }
                 }
                 video_is_playing = false;
-                cp.exec("export DISPLAY=:0", function(error, stdout, stderr) {});
-                //omx.quit();
-                omx.open("http://10.3.141.1:3000/"+datas.file, omx_options);
-                omx.setVolume(app_volume);
-                io.emit(call, {"status":"video_started", "duration":omx.getCurrentDuration(), "position":omx.getCurrentPosition()});
-                resetProgressListener();
+                omx.quit();
+                setTimeout(function(){
+                  cp.exec("export DISPLAY=:0", function(error, stdout, stderr) {});
+                  omx.open("http://10.3.141.1:3000/"+datas.file, omx_options);
+                  omx.setVolume(app_volume);
+                  io.emit(call, {"status":"video_started", "duration":omx.getCurrentDuration(), "position":omx.getCurrentPosition()});
+                  resetProgressListener();
+                }, 200);
                 break;
             case 'pause_video':
                 omx.pause();
