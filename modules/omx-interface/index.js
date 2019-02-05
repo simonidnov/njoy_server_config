5016076009c60412bc1628ca54d2b28ddb0f59cb
simonidnov <sdelamarre@idnovant.fr>
2019-02-05 17:53:48 +0100

    upgrade packages omx player

--- modules/omx-interface/index.js
+++ modules/omx-interface/index.js
@@ -408,6 +408,7 @@ var open = function (path, options) {
 
 	args.push('--dbus_name');
 	args.push('org.mpris.MediaPlayer2.omxplayer');
+	console.log(command+' '+args.join(' ')+' < omxpipe');
   exec(command+' '+args.join(' ')+' < omxpipe',function(error, stdout, stderr) {
 		update_duration();
 		setTimeout( function() {
