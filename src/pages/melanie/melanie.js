b7eada6f8d692c988761f088188e5fa603070d79
Simon Delamarre <sdelamarre@lmacidn0044.d01.local>
2019-08-08 12:22:26 +0200

    update labofolies

--- src/pages/melanie/melanie.js
+++
@@ -1,9 +0,0 @@
-function melanie() {
-    console.log('hello melanie');
-}
-melanie.init = function () {
-    console.log('je suis prête');
-}
-melanie.sayHello = function () {
-    alert('HELLO');
-}
